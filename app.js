f08d1974365f5ffb7dc9f1e3c5a1ae04adb33cb3
Hoccyy <94509399+Hoccyy@users.noreply.github.com>
2022-02-13 16:07:11 -0500

    Basic Prototype

---
+++ app.js
@@ -0,0 +1,10 @@
+/* Gets the elements from html */
+const temperature = document.getElementById("temp-val");
+const tempDescription = document.getElementById("temp-desc");
+const locat = document.getElementById("loc");
+
+/* Manipulates the elements */
+temperature.innerHTML = " 23 ° ";
+tempDescription.innerHTML = " Sunny ";
+locat.innerHTML = " Britain idk ";
+
